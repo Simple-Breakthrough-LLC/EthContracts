5ec7115776dc0533bff4884fcb43f4a03be3f5ed
flavorlessquark <flavorlessquark@gmail.com>
2022-11-03 14:58:45 -0700

    Auction

--- RadMarketplace/contracts/marketplace.sol
+++ RadMarketplace/contracts/marketplace.sol
@@ -19,6 +19,8 @@ contract MarketPlace is Ownable, IERC721Receiver {
 
 	struct auction {
 		sale data;
+		uint256 startTime;
+		uint256 duration;
 		address bidder;
 		uint256 bid;
 	}
@@ -33,8 +35,10 @@ contract MarketPlace is Ownable, IERC721Receiver {
 	uint256 waiver;
 
 	uint256 _salesId;
+	uint256 _auctionId;
 
 	mapping (uint256 => sale) internal _sales;
+	mapping (uint256 => auction) internal _auctions;
 
 	constructor (
 		address _ara,
@@ -44,6 +48,7 @@ contract MarketPlace is Ownable, IERC721Receiver {
 		araToken = _ara;
 		marketPlaceFee = _marketFee;
 		_salesId = 1;
+		_auctionId = 1;
 		waiver = _waiver;
 	}
 
@@ -61,6 +66,10 @@ contract MarketPlace is Ownable, IERC721Receiver {
 
 	function getSimpleSale(uint256 saleId) external view returns (uint256) {
 		return _sales[saleId].price;
+	}
+
+	function getAuction(uint256 auctionId) external view returns (address, uint256) {
+		return (_auctions[auctionId].bidder, _auctions[auctionId].bid);
 	}
 
 	function onERC721Received(address,address,uint256,bytes calldata) external pure returns (bytes4) {
@@ -112,8 +121,37 @@ contract MarketPlace is Ownable, IERC721Receiver {
 		delete _sales[saleId];
 	}
 
-	function createAuction (address nftContract, uint256 nftId, uint256 price, uint256 startTime, uint256 duration) external {
+	function createAuction (address nftContract, uint256 nftId, uint256 price, uint256 startTime, uint256 duration) external returns (uint256) {
+		// expecting time between when user submits to time when transaction is ran to be different
+		require(startTime + 60 >= block.timestamp, "Duration must be greater than 0");
 
+		ERC721 nft = ERC721(nftContract);
+
+		nft.transferFrom(msg.sender, address(this), nftId);
+		_auctions[_auctionId] = auction(sale(nftContract, payable(msg.sender), nftId, price), startTime, duration, address(0x0), 0);
+
+
+		_auctionId++;
+		return _auctionId - 1;
 	}
 
+
+	function bidAuction(uint256 auctionId) external payable {
+		require(block.timestamp < (_auctions[auctionId].startTime + _auctions[auctionId].duration));
+		require(msg.value > _auctions[auctionId].bid);
+
+		if (_auctions[auctionId].bidder != address(0x0))
+			payable(_auctions[auctionId].bidder).transfer(_auctions[auctionId].bid);
+
+		_auctions[auctionId].bid = msg.value;
+		_auctions[auctionId].bidder = msg.sender;
+	}
+
+	function redeemAuction(uint256 auctionId) external {
+		require(msg.sender == _auctions[auctionId].bidder);
+
+		ERC721 nft = ERC721(_auctions[auctionId].data.contractAddress);
+
+		nft.transferFrom(address(this), msg.sender, _auctions[auctionId].data.id);
+	}
 }
